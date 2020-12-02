e869c2ac2f50d6dfcfb616d8f4e45503d4ec582d
jayudhandha <jayudhandha@gmail.com>
2020-12-02 21:54:22 +0530

    first commit

---
+++ src/app/my-textbox/my-textbox.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-textbox',
+  templateUrl: './my-textbox.component.html',
+  styleUrls: ['./my-textbox.component.css']
+})
+export class MyTextboxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  name = ''
+
+  onClick() {
+    window.alert("Clicked")
+  }
+}
